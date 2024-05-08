46d3e68793306be1afafe50786337dd40a9f6a50
Alex Yang <alex.hyang.dev@gmail.com>
2024-05-08 14:29:40 -0700

    add vscode symlink command

--- createSymLinks-runAsAdmin.bat
+++ createSymLinks-runAsAdmin.bat
@@ -4,6 +4,7 @@ REM This batch file creates symbolic links for multiple files in the current dir
 REM Set the source directory to the current directory of the batch file
 set "homeDir=%USERPROFILE%"
 set "dFilesDir=%homeDir%\.dotfiles"
+set "vscodeUserDir=%homeDir%\AppData\Roaming\Code\User"
 
 REM Create symbolic links for each file
 mklink "%homeDir%\.bash_aliases" "%dFilesDir%\.bash_aliases"
@@ -11,6 +12,8 @@ mklink "%homeDir%\.bashrc" "%dFilesDir%\.bashrc"
 mklink "%homeDir%\.gitconfig" "%dFilesDir%\.gitconfig"
 mklink "%homeDir%\.ideavimrc" "%dFilesDir%\.ideavimrc"
 mklink "%homeDir%\_vimrc" "%dFilesDir%\_vimrc"
+mklink "%homeDir%\.vscodevimrc" "%dFilesDir%\.vscodevimrc"
+mklink "%APPDATA%\Code\User\settings.json" "%dFilesDir%\vscode-userSettings.json"
 
 REM Add more lines if you have additional files to link
 
