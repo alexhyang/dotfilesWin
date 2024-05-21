8e90e99a6f2865bf0749188023d270c52be070c7
Alex Yang <alex.hyang.dev@gmail.com>
2024-05-21 04:40:05 -0700

    add sublime text user settings

--- createSymLinks-runAsAdmin.bat
+++ createSymLinks-runAsAdmin.bat
@@ -4,7 +4,7 @@ REM This batch file creates symbolic links for multiple files in the current dir
 REM Set the source directory to the current directory of the batch file
 set "homeDir=%USERPROFILE%"
 set "dFilesDir=%homeDir%\.dotfiles"
-set "vscodeUserDir=%homeDir%\AppData\Roaming\Code\User"
+REM set "APPDATA=%homeDir%\AppData\Roaming"
 
 REM Create symbolic links for each file
 mklink "%homeDir%\.bash_aliases" "%dFilesDir%\.bash_aliases"
@@ -13,7 +13,8 @@ mklink "%homeDir%\.gitconfig" "%dFilesDir%\.gitconfig"
 mklink "%homeDir%\.ideavimrc" "%dFilesDir%\.ideavimrc"
 mklink "%homeDir%\_vimrc" "%dFilesDir%\_vimrc"
 mklink "%homeDir%\.vscodevimrc" "%dFilesDir%\.vscodevimrc"
-mklink "%APPDATA%\Code\User\settings.json" "%dFilesDir%\vscode-userSettings.json"
+mklink "%APPDATA%\Code\User\settings.json" "%dFilesDir%\vscode-settings.json"
+mklink "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%dFilesDir%\.sublime-settings"
 
 REM Add more lines if you have additional files to link
 
