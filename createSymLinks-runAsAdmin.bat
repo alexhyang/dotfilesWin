c4f3fe3d7bfdf2be83900f139039bc0d75558ae1
Alex Yang <alex.hyang.dev@gmail.com>
2024-05-24 02:10:12 -0700

    refactor

--- createSymLinks-runAsAdmin.bat
+++ createSymLinks-runAsAdmin.bat
@@ -3,18 +3,18 @@ REM This batch file creates symbolic links for multiple files in the current dir
 
 REM Set the source directory to the current directory of the batch file
 set "homeDir=%USERPROFILE%"
-set "dFilesDir=%homeDir%\.dotfiles"
+set "dotfilesDir=%homeDir%\.dotfiles"
 REM set "APPDATA=%homeDir%\AppData\Roaming"
 
 REM Create symbolic links for each file
-mklink "%homeDir%\.bash_aliases" "%dFilesDir%\.bash_aliases"
-mklink "%homeDir%\.bashrc" "%dFilesDir%\.bashrc"
-mklink "%homeDir%\.gitconfig" "%dFilesDir%\.gitconfig"
-mklink "%homeDir%\.ideavimrc" "%dFilesDir%\.ideavimrc"
-mklink "%homeDir%\_vimrc" "%dFilesDir%\_vimrc"
-mklink "%homeDir%\.vscodevimrc" "%dFilesDir%\.vscodevimrc"
-mklink "%APPDATA%\Code\User\settings.json" "%dFilesDir%\vscode-settings.json"
-mklink "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%dFilesDir%\.sublime-settings"
+mklink "%homeDir%\.bash_aliases" "%dotfilesDir%\.bash_aliases"
+mklink "%homeDir%\.bashrc" "%dotfilesDir%\.bashrc"
+mklink "%homeDir%\.gitconfig" "%dotfilesDir%\.gitconfig"
+mklink "%homeDir%\.ideavimrc" "%dotfilesDir%\.ideavimrc"
+mklink "%homeDir%\_vimrc" "%dotfilesDir%\_vimrc"
+mklink "%homeDir%\.vscodevimrc" "%dotfilesDir%\.vscodevimrc"
+mklink "%APPDATA%\Code\User\settings.json" "%dotfilesDir%\vscode-settings.json"
+mklink "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%dotfilesDir%\.sublime-settings"
 
 REM Add more lines if you have additional files to link
 
