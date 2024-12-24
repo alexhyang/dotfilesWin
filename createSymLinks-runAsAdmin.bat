3efd6d6ef6fcf73496e5d36ae59c1a2276b47159
Alex Yang <alex.hyang.dev@gmail.com>
2024-12-24 01:33:10 -0800

    update config files

--- createSymLinks-runAsAdmin.bat
+++ createSymLinks-runAsAdmin.bat
@@ -4,7 +4,9 @@ REM This batch file creates symbolic links for multiple files in the current dir
 REM Set the source directory to the current directory of the batch file
 set "homeDir=%USERPROFILE%"
 set "dotfilesDir=%homeDir%\.dotfiles"
-REM set "APPDATA=%homeDir%\AppData\Roaming"
+set "APPDATA=%homeDir%\AppData"
+set "Local=%APPDATA%\Local"
+set "Roaming=%APPDATA%\Roaming"
 
 REM Create symbolic links for each file
 REM mklink <link> <target>
@@ -15,9 +17,10 @@ mklink "%homeDir%\.gitconfig" "%dotfilesDir%\.gitconfig"
 mklink "%homeDir%\.ideavimrc" "%dotfilesDir%\.ideavimrc"
 mklink "%homeDir%\_vimrc" "%dotfilesDir%\_vimrc"
 mklink "%homeDir%\.vscodevimrc" "%dotfilesDir%\.vscodevimrc"
-mklink "%APPDATA%\Code\User\settings.json" "%dotfilesDir%\vscode-settings.json"
-mklink "%APPDATA%\Code\User\snippets\markdown.json" "%dotfilesDir%\vscode-snippets-markdown.json"
-mklink "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%dotfilesDir%\.sublime-settings"
+mklink "%Romaing%\Code\User\settings.json" "%dotfilesDir%\vscode-settings.json"
+mklink "%Romaing%\Code\User\snippets\markdown.json" "%dotfilesDir%\vscode-snippets-markdown.json"
+mklink "%Romaing%\Sublime Text\Packages\User\Preferences.sublime-settings" "%dotfilesDir%\.sublime-settings"
+mklink "%Local%\nvim\init.vim" "%dotfilesDir%\nvimInit.vim"
 
 REM Add more lines if you have additional files to link
 
