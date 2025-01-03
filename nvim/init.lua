3b4788cab26a25d9f5088922009d9e8e3c71d7b4
Alex Yang <alex.hyang.dev@gmail.com>
2025-01-02 18:21:46 -0800

    fix cmp bug

--- nvim/init.lua
+++ nvim/init.lua
@@ -468,6 +468,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 -- cmp
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -479,33 +480,33 @@ cmp.setup({
     -- completion = cmp.config.window.bordered(),
     -- documentation = cmp.config.window.bordered(),
   },
-  -- mapping = cmp.mapping.preset.insert({
-  --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --   ['<C-Space>'] = cmp.mapping.complete(),
-  --   ['<C-e>'] = cmp.mapping.abort(),
-  --   ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  --   ["<Tab>"] = cmp.mapping(function(fallback)
-  --     if cmp.visible() then
-  --       cmp.select_next_item()
-  --     elseif luasnip.expandable() then
-  --       luasnip.expand()
-  --     elseif luasnip.expand_or_jumpable() then
-  --       luasnip.expand_or_jump()
-  --     else
-  --       fallback()
-  --     end
-  --   end, { "i", "s", }),
-  --   ["<S-Tab>"] = cmp.mapping(function(fallback)
-  --     if cmp.visible() then
-  --       cmp.select_prev_item()
-  --     elseif luasnip.jumpable(-1) then
-  --       luasnip.jump(-1)
-  --     else
-  --       fallback()
-  --     end
-  --   end, { "i", "s", }),
-  -- }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s", }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s", }),
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' }, -- For luasnip users.
